bb67e8c8f4ab3bb08f933a99b7398fd32734f0b4
Pacatro <paco.algar.15@gmail.com>
2023-03-05 01:07:58 +0100

    no hace una tab por la cara en ej4 :(

--- p1/ej4/ej4.sh
+++ p1/ej4/ej4.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+listarFicheros(){
+    for files in $(find $dir -type f -name "*.$ext"); do
+        echo -e "$(basename $files)\t$(grep -o '.' $files | wc -l)\t$(grep -o $char $files | wc -l)" >> files.txt  
+    done
+}
+
 if [ $# != 2 ]; then
-    echo "Argumentos incorrectos. Uso: ./ejercicio4.sh <ruta_directorio> <extension_fichero>"
+    echo -e "Argumentos incorrectos.\nUso: ./ejercicio4.sh <ruta_directorio> <extension_fichero>"
 fi
 
-file=$(basename $1)
+dir=$1
 ext=$2
 
-echo $file
+echo -e "Caracter a contar: "
+read char
 
+echo -e "\nFicheros:"
+listarFicheros $char
+nl files.txt
+rm files.txt
