83ac0e3ef8402b61494cbe267373d89064d850a7
Pacatro <paco.algar.15@gmail.com>
2023-03-08 10:05:56 +0100

    P1 ACABADA

---
+++ p1/ej6/ej6.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+mover_archivos(){
+    cd ../skel
+    for file in $(ls); do
+        cp $file ../homes/$user
+    done
+    cd ..
+}
+
+crear_usuario(){
+    cd homes/
+    
+    if [ -d $user ]; then
+        echo -e "El usuario $user ya existe\nSaliendo..."
+        exit
+    fi
+
+    mkdir $user
+    mover_archivos $user
+
+    echo $user >> users.txt
+}
+
+if [ $# != 1 ]; then
+    echo -e "Argumentos incorrectos.\nUso: ./ejercicio6.sh <usuario>"
+    exit
+fi
+
+user=$1
+
+if !([ -f users.txt ]); then 
+    touch users.txt
+fi
+
+if !([ -d homes ]); then
+    mkdir homes
+fi
+
+crear_usuario $user
