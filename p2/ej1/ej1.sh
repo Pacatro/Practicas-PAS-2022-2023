11e2f8794dfccbfa96525f7494229b15d51509fe
Pacatro <paco.algar.15@gmail.com>
2023-03-12 18:21:08 +0100

    poco a poco

--- p2/ej1/ej1.sh
+++ p2/ej1/ej1.sh
@@ -15,4 +15,8 @@ echo -e "1) Lineas con la duracion de las peliculas:"
 echo -e "$(cat $fich | grep '^[1-9]')\n"
 
 echo -e "2) Solo pais de las peliculas:"
-echo -e "$(cat peliculas.txt | grep -o '\-[^\-]*\-')\n"
+echo -e "$(cat $fich | grep -o '\-[^\-]*\-')\n"
+
+echo -e "3) Hay $(cat $fich | grep -o -c '\(/2016\)') películas de 2016 y $(cat $fich | grep -o -c '\(/2017\)') del 2017"
+
+
