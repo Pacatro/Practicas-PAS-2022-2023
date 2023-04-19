dfaf415fec66cea11df879daac7c86f70d952746
Pacatro <paco.algar.15@gmail.com>
2023-04-20 01:47:12 +0200

    falta -s y -h

--- p3/ej1/ej1.c
+++ p3/ej1/ej1.c
@@ -1,10 +1,15 @@
 #include <pwd.h>
+#include <grp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <ctype.h>
+
+void printUserInfo(char *lgn);
+void printGroupInfo(char *lgn);
 
 int main(int argc, char **argv){
     int command;
@@ -27,24 +32,50 @@ int main(int argc, char **argv){
     char *uvalue = NULL;
     char *gvalue = NULL;
 
-    while((command = getopt_long(argc, argv, "u:g:amsh", long_options, NULL)) != -1){
-        printf("optind: %d, optarg: %s, optopt: %c\n", optind, optarg, optopt);
+    command = getopt_long(argc, argv, "u:g:amsh", long_options, NULL);
+
+    if(command != -1){
+        //printf("optind: %d, optarg: %s, optopt: %c\n", optind, optarg, optopt);
 
         switch(command){
             case 'u':
                 uvalue = optarg;
+                printUserInfo(uvalue);
             break;
 
             case 'g':
                 gvalue = optarg;
+                printGroupInfo(gvalue);
             break;
 
             case 'a':
                 aflag = true;
+                
+                if(aflag){
+                    char *lgn = getenv("USER");
+                    
+                    if(lgn == NULL || lgn == ""){
+                        fprintf(stderr, "Failed to get user information.\n");
+                        exit(1);
+                    }
+
+                    printUserInfo(lgn);
+                }
             break;
 
             case 'm':
                 mflag = true;
+
+                if(mflag){
+                    char *lgn = getenv("USER");
+                    
+                    if(lgn == NULL){
+                        fprintf(stderr, "Failed to get group information.\n");
+                        exit(1);
+                    }
+
+                    printGroupInfo(lgn);
+                }
             break;
 
             case 's':
@@ -66,4 +97,73 @@ int main(int argc, char **argv){
     
     return 0;
     
+}
+
+void printUserInfo(char *lgn){
+    struct passwd *pw;
+
+    for(int i = 0; lgn[i]; i++){
+        if(!isalpha(lgn[i])){
+            int uid = atoi(lgn);
+            pw = getpwuid(uid);
+        } else { 
+            pw = getpwnam(lgn);
+        }
+    }
+
+    /* Tambien funciona así
+    
+    if(!isalpha(lgn[1])){
+        int uid = atoi(lgn);
+        pw = getpwuid(uid);
+    } else { 
+        pw = getpwnam(lgn);
+    }
+    */
+
+    if(pw == NULL){
+        fprintf(stderr, "Failed to get user information.\n");
+        exit(1);
+    }
+
+    printf("Name: %s\n", pw->pw_gecos);
+    printf("Login: %s\n", pw->pw_name);
+    printf("Password: %s\n", pw->pw_passwd);
+    printf("UID: %d\n", pw->pw_uid);
+    printf("Home: %s\n", pw->pw_dir);
+    printf("Shell: %s\n", pw->pw_shell);
+    printf("Principal group number %d\n", pw->pw_gid);
+}
+
+void printGroupInfo(char *lgn){
+    struct group *gr;
+    
+    for(int i = 0; lgn[i]; i++){
+        if(!isalpha(lgn[i])){
+            int gid = atoi(lgn);
+            gr = getgrgid(gid);
+        } else { 
+            gr = getgrnam(lgn);
+        }
+    }
+
+    /* Tambien funciona así
+    
+    if(!isalpha(lgn[1])){
+        int gid = atoi(lgn);
+        gr = getgrgid(gid);
+    } else { 
+        gr = getgrnam(lgn);
+    }
+    */
+
+    if(gr == NULL){
+        fprintf(stderr, "Failed to get group information.\n");
+        exit(1);
+    }
+
+    printf("Name: %s\n", gr->gr_name);
+    printf("Password: %s\n", gr->gr_passwd);
+    printf("GID: %d\n", gr->gr_gid);
+    //printf("Member list: %s\n", gr->gr_mem[1]); --> necesario?
 }
